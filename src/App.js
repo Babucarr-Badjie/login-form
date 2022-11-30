5ea940cbb3b467b49222251a0b5993f7bed95ba3
Babucarr-Badjie <babucarr.badjie12@gmail.com>
2022-11-30 22:04:12 +0800

    image added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import logo from "./images/user-circle.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-content">
+      <div className="content">
+        <div className="form-content">
+          <div className="image-container">
+            <div className="image-logo">
+              <img src={logo} alt="" />
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

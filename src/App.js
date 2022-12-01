26c9a4c34a449f639ee87670cd2a5efc6aa20e2e
Babucarr-Badjie <babucarr.badjie12@gmail.com>
2022-12-01 22:06:07 +0800

    form created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./index.css";
-import logo from "./images/user-circle.svg"
+import userProfile from "./images/user.png";
+import userEmail from "./images/userEmail.jpg";
+import userPassword from "./images/userPass.png";
 
 function App() {
   return (
@@ -8,9 +10,31 @@ function App() {
         <div className="form-content">
           <div className="image-container">
             <div className="image-logo">
-              <img src={logo} alt="" />
-
+              <img
+                src={userProfile}
+                alt="userProfile"
+                className="user-profile"
+              />
             </div>
+          </div>
+          <div>
+            <h1>Login Form</h1>
+          </div>
+          <div>
+            <img src={userEmail} alt="userEmail" className="user-email" />
+            <input type="text" placeholder="user name" className="user-name" />
+          </div>
+          <div className="input-two">
+            <img src={userPassword} alt="userPassword" className="user-email" />
+            <input type="password" placeholder="user password" className="user-name" />
+          </div>
+          <div className="login-button">
+            <button>Login</button>
+          </div>
+          <div>
+            <p className="links">
+              <a href="#">Forgot password</a> or <a href="#">Sign up</a>
+            </p>
           </div>
         </div>
       </div>
